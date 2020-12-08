24fa1a7466b12324df0d9f56044cfbd9e1fa4178
bastienwcs <bastien.krettly@wildcodeschool.com>
2020-12-08 14:55:10 +0100

    init

---
+++ index.js
@@ -0,0 +1,55 @@
+const { request } = require('express');
+const express = require('express');
+const app = express();
+const port = 3001; // 80,8080,3000,3306
+
+const connection = require('./config');
+
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+app.listen(port, () => {
+  console.log(`port ${port} OK`);
+});
+
+app.get('/restaurants', (request, response) => {
+  connection.query('SELECT * FROM restaurant', (error, result) => {
+    if (error) {
+      response.status(500).json({
+        error: error,
+      });
+    } else {
+      response.status(200).json({
+        data: result,
+      });
+    }
+  });
+});
+
+app.get('/restaurants/:id', (request, response) => {
+  const { id } = request.params;
+  connection.query(
+    `SELECT * FROM restaurant WHERE id = ${id}`,
+    (error, result) => {
+      if (error) {
+        response.status(500).json({
+          error: error,
+        });
+      } else if (result.length === 0) {
+        response.status(404).json({
+          error: `restaurant ${id} not found`,
+        });
+      } else {
+        response.status(200).json({
+          data: result[0],
+        });
+      }
+    }
+  );
+});
